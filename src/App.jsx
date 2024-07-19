bd72516c49102a87488f30043228e0d478f1e5dc
wajahatkarimFASTIAN <wajahatkarim400@outlook.com>
2024-07-19 11:33:45 +0500

    Finished Project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Footer from "./components/Footer"
+import Analytics from "./components/Analytics"
+import Cards from "./components/Cards"
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Analytics />
+      <Cards />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex md:cursor-pointer'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li><button className=' md:bg-[#00df9a] md:w-[150px] md:rounded-md md:font-medium md:mx-auto md:py-3 md:ml-4 md:mt-1 md:text-black'>Contact Us</button></li>
+      </ul>
+      <div onClick={handleNav} className='block cursor-pointer md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+      </div>
+      <ul className={nav ? 'fixed left-0 top-2 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
